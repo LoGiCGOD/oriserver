d44e7fb7acfe2ec2282d90b76745859281fb182a
LoGiCGOD <adutta786610@gmail.com>
2024-09-09 09:21:53 +0530

    added scripts

---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/application
+sudo npm install
+sudo pm2 start app.js
+
--- scripts/unzip_and_prepare.sh
+++ scripts/unzip_and_prepare.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
-
-cd /home/ubuntu/application
-unzip application.zip
-# chmod +x start_server.sh
+if [ ! -d /home/ubuntu/application ]; then
+    echo "Fresh EC2 Instance"
+    exit 0
+else
+    cd /home/ubuntu/application
+    sudo pm2 stop app.js
+    sudo pm2 delete app.js
+    cd /home/ubuntu && sudo rm -rf application
+fi
